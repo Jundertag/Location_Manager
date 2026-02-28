706d24b241c39c8918d160d043c6ab8ec12726d6
Jundertag <jaydenmacarthur3@gmail.com>
2026-02-28 10:39:30 -0400

    add DAO delete function

--- app/src/main/java/com/jayden/locationmanager/data/source/room/dao/NmeaEventDao.kt
+++ app/src/main/java/com/jayden/locationmanager/data/source/room/dao/NmeaEventDao.kt
@@ -1,6 +1,7 @@
 package com.jayden.locationmanager.data.source.room.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import com.jayden.locationmanager.data.source.room.entity.NmeaEventEntity
 
@@ -8,4 +9,9 @@ import com.jayden.locationmanager.data.source.room.entity.NmeaEventEntity
 interface NmeaEventDao {
     @Insert
     fun insertAll(vararg events: NmeaEventEntity)
+
+    @Delete
+    fun delete(event: NmeaEventEntity)
+
+
 }
