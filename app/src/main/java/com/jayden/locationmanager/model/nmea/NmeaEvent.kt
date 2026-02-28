486f93ab269d84e269f5a6078b59919629c2e272
Jundertag <jaydenmacarthur3@gmail.com>
2026-02-28 19:13:11 -0400

    add NmeaSentence arg to NmeaEvent.kt. add sentence translator function helper to TypeConverter.kt

--- app/src/main/java/com/jayden/locationmanager/model/nmea/NmeaEvent.kt
+++ app/src/main/java/com/jayden/locationmanager/model/nmea/NmeaEvent.kt
@@ -2,29 +2,8 @@ package com.jayden.locationmanager.model.nmea
 
 data class NmeaEvent(
     val talkerId: TalkerId,
+    val sentenceType: NmeaSentence,
     val message: NmeaMessage
 ) {
-    fun getPrettySentenceType(): String = when (message) {
-        is NmeaMessage.AamEvent -> "AAM - Waypoint Arrival Alarm"
-        is NmeaMessage.AlmEvent -> "ALM - GPS Almanac Data"
-        is NmeaMessage.ApaEvent -> "APA - Autopilot Sentence \"A\""
-        is NmeaMessage.ApbEvent -> "APB - Autopilot Sentence \"B\""
-        is NmeaMessage.BodEvent -> "BOD - Bearing - Waypoint to Waypoint"
-        is NmeaMessage.BwcEvent -> "BWC - Bearing & Distance to Waypoint - Great Circle"
-        is NmeaMessage.BwrEvent -> "BWR - Bearing and Distance to Waypoint - Rhumb Line"
-        is NmeaMessage.BwwEvent -> "BWW - Bearing - From Waypoint to Waypoint"
-        is NmeaMessage.DbkEvent -> "DBK - Depth Below Keel"
-        is NmeaMessage.DbsEvent -> "DBS - Depth Below Surface"
-        is NmeaMessage.DbtEvent -> "DPT - Depth below transducer"
-        is NmeaMessage.DptEvent -> "DPT - Depth of Water"
-        is NmeaMessage.DtmEvent -> "DTM - datum Reference"
-        is NmeaMessage.FsiEvent -> "FSI - Frequency Set Information"
-        is NmeaMessage.GbsEvent -> "GBS - GPS Satellite Fault Detection"
-        is NmeaMessage.GgaEvent -> "GGA - Global Positioning System Fix Data"
-        is NmeaMessage.GllEvent -> "GLL - Geographic Position - Latitude/Longitude"
-        is NmeaMessage.GnsEvent -> "GNS - Fix data"
-        is NmeaMessage.GrsEvent -> "GRS - GPS Range Residuals"
-        is NmeaMessage.ProprietaryEvent -> "Proprietary Event"
-        is NmeaMessage.RawEvent -> "Raw Event"
-    }
+    fun getPrettySentenceType(): String = sentenceType.toString()
 }
--- app/src/main/java/com/jayden/locationmanager/model/nmea/typeconverter/TypeConverter.kt
+++ app/src/main/java/com/jayden/locationmanager/model/nmea/typeconverter/TypeConverter.kt
@@ -6,7 +6,8 @@ import com.jayden.locationmanager.model.nmea.NmeaSentence
 import com.jayden.locationmanager.model.nmea.TalkerId
 
 object TypeConverter {
-    private val TalkerIdRegex = Regex("""^[$!].{2}""")
+    private val TalkerIdRegex = Regex("""^[$!](.{2})""")
+    private val SentenceTypeRegex = Regex("""^[$!].{2}(.{3})""")
 
     fun String?.parseTalkerId(): TalkerId = when (this) {
         "AB" -> TalkerId.INDEPENDENT_AIS
@@ -54,21 +55,102 @@ object TypeConverter {
     }
 
     fun String?.parseSentence(): NmeaSentence = when (this) {
+        "AAM" -> NmeaSentence.WAYPOINT_ARRIVAL_ALARM
+        "ALM" -> NmeaSentence.GPS_ALMANAC_DATA
+        "APA" -> NmeaSentence.AUTOPILOT_SENTENCE_A
+        "APB" -> NmeaSentence.AUTOPILOT_SENTENCE_B
+        "BOD" -> NmeaSentence.BEARING_WAYPOINT_TO_WAYPOINT
+        "BWC" -> NmeaSentence.BEARING_DISTANCE_TO_WAYPOINT_CIRCLE
+        "BWR" -> NmeaSentence.BEARING_DISTANCE_TO_WAYPOINT_LINE
+        "DBK" -> NmeaSentence.DEPTH_BELOW_KEEL
+        "DBS" -> NmeaSentence.DEPTH_BELOW_SURFACE
+        "DBT" -> NmeaSentence.DEPTH_BELOW_TRANSDUCER
+        "DPT" -> NmeaSentence.DEPTH_OF_WATER
+        "DTM" -> NmeaSentence.DATUM_REFERENCE
+        "FSI" -> NmeaSentence.FREQUENCY_SET_INFORMATION
+        "GBS" -> NmeaSentence.GPS_SATELLITE_FAULT_DETECTION
+        "GGA" -> NmeaSentence.GLOBAL_POSITIONING_SYSTEM_FIX_DATA
+        "GLC" -> NmeaSentence.GEOGRAPHIC_POSITION
+        "GNS" -> NmeaSentence.FIX_DATA
+        "GRS" -> NmeaSentence.GPS_RANGE_RESIDUALS
+        "GST" -> NmeaSentence.GPS_PSEUDORANGE_NOISE_STATISTICS
+        "GSA" -> NmeaSentence.GPS_DOP_AND_ACTIVE_SATELLITES
+        "GSV" -> NmeaSentence.SATELLITES_IN_VIEW
+        "GTD" -> NmeaSentence.GEOGRAPHIC_LOCATION_TIME_DIFFERENCES
+        "GXA" -> NmeaSentence.TRANSIT_POSITION
+        "HDG" -> NmeaSentence.HEADING_DEVIATION_VARIATION
+        "HDM" -> NmeaSentence.HEADING_MAGNETIC
+        "HDT" -> NmeaSentence.HEADING_TRUE
+        "HFB" -> NmeaSentence.TRAWL_HEADROPE_TO_FOOTROPE_AND_BOTTOM
+        "HSC" -> NmeaSentence.HEADING_STEERING_COMMAND
+        "ITS" -> NmeaSentence.TRAWL_DOOR_SPREAD_2_DISTANCE
+        "MSK" -> NmeaSentence.CONTROL_BEACON_RECEIVER
+        "MSS" -> NmeaSentence.BEACON_RECEIVER_STATUS
+        "MTW" -> NmeaSentence.MEAN_TEMPERATURE_OF_WATER
+        "MWD" -> NmeaSentence.WIND_DIRECTION_AND_SPEED
+        "MWV" -> NmeaSentence.WIND_SPEED_AND_ANGLE
+        "OSD" -> NmeaSentence.OWN_SHIP_DATA
+        "R00" -> NmeaSentence.WAYPOINTS_IN_ACTIVE_ROUTE
+        "RLM" -> NmeaSentence.RETURN_LINK_MESSAGE
+        "RMA" -> NmeaSentence.RECOMMENDED_MINIMUM_NAVIGATION_INFORMATION
+        "RMB" -> NmeaSentence.RECOMMENDED_MINIMUM_NAVIGATION_RECEIVER_INFORMATION
+        "RMC" -> NmeaSentence.RECOMMENDED_MINIMUM_NAVIGATION_GPS_INFORMATION
+        "ROT" -> NmeaSentence.RATE_OF_TURN
+        "RPM" -> NmeaSentence.REVOLUTIONS
+        "RSA" -> NmeaSentence.RUDDER_SENSOR_ANGLE
+        "RSD" -> NmeaSentence.RADAR_SYSTEM_DATA
+        "RTE" -> NmeaSentence.ROUTES
+        "SFI" -> NmeaSentence.SCANNING_FREQUENCY_INFORMATION
+        "STN" -> NmeaSentence.MULTIPLE_DATA_ID
+        "TDS" -> NmeaSentence.TRAWL_DOOR_SPREAD_DISTANCE
+        "TFI" -> NmeaSentence.TRAWL_FILLING_INDICATOR
+        "TLB" -> NmeaSentence.TARGET_LABEL
+        "TLL" -> NmeaSentence.TARGET_LATITUDE_LONGITUDE
+        "TPC" -> NmeaSentence.TRAWL_POSITION_CARTESIAN_COORDINATES
+        "TPR" -> NmeaSentence.TRAWL_POSITION_RELATIVE_VESSEL
+        "TPT" -> NmeaSentence.TRAWL_POSITION_TRUE
+        "TRF" -> NmeaSentence.TRANSIT_FIX_DATA
+        "TTM" -> NmeaSentence.TRACKED_TARGET_MESSAGE
+        "VBW" -> NmeaSentence.DUAL_GROUND_WATER_SPEED
+        "VDR" -> NmeaSentence.SET_AND_DRIFT
+        "VHW" -> NmeaSentence.WATER_SPEED_AND_HEADING
+        "VLW" -> NmeaSentence.DISTANCED_TRAVELLED_THROUGH_WATER
+        "VPW" -> NmeaSentence.SPEED_PARALLEL_TO_WIND
+        "VTG" -> NmeaSentence.TRACK_GROUND_SPEED
+        "VWR" -> NmeaSentence.RELATIVE_WIND_SPEED_AND_ANGLE
+        "WCV" -> NmeaSentence.WAYPOINT_CLOSURE_VELOCITY
+        "WNC" -> NmeaSentence.DISTANCE_WAYPOINT_TO_WAYPOINT
+        "WPL" -> NmeaSentence.WAYPOINT_LOCATION
+        "XDR" -> NmeaSentence.TRANSDUCER_MEASUREMENT
+        "XTE" -> NmeaSentence.CROSS_TRACK_MEASURED_ERROR
+        "XTR" -> NmeaSentence.CROSS_TRACK_DEAD_RECKONING_ERROR
+        "ZDA" -> NmeaSentence.TIME_AND_DATE
+        "ZFO" -> NmeaSentence.TIME_FROM_ORIGIN_WAYPOINT
+        "ZTG" -> NmeaSentence.TIME_TO_DESTINATION_WAYPOINT
         null -> NmeaSentence.UNKNOWN
-        else -> NmeaSentence.UNKNOWN
+        else -> NmeaSentence.PROPRIETARY_EVENT
     }
 
     fun getTalkerId(input: String): TalkerId {
         return if (input.contains(Regex("""^[$!]P"""))) {
             TalkerId.PROPRIETARY
         } else {
-            TalkerIdRegex.find(input)?.value?.drop(1).parseTalkerId()
+            TalkerIdRegex.find(input)?.groupValues?.getOrNull(1).parseTalkerId()
+        }
+    }
+
+    fun getSentenceType(input: String): NmeaSentence {
+        return if (input.contains(Regex("""^[$!]P"""))) {
+            NmeaSentence.PROPRIETARY_EVENT
+        } else {
+            SentenceTypeRegex.find(input)?.groupValues?.getOrNull(1).parseSentence()
         }
     }
 
     fun String.toNmeaEvent(): NmeaEvent {
         return NmeaEvent(
             talkerId = getTalkerId(this),
+            sentenceType = getSentenceType(this),
             message = NmeaMessage.RawEvent(this)
         )
     }
