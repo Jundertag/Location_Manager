3ecab350e5c69491977505032f990d18246118d5
Jundertag <jaydenmacarthur3@gmail.com>
2026-02-28 11:11:36 -0400

    add database object access method

--- app/src/main/java/com/jayden/locationmanager/data/source/NmeaLogsDatabase.kt
+++ app/src/main/java/com/jayden/locationmanager/data/source/NmeaLogsDatabase.kt
@@ -2,7 +2,9 @@ package com.jayden.locationmanager.data.source
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.jayden.locationmanager.data.source.room.dao.NmeaEventDao
 
 @Database(version = 1, entities = [])
 abstract class NmeaLogsDatabase : RoomDatabase() {
+    abstract fun nmeaEventDao(): NmeaEventDao
 }
