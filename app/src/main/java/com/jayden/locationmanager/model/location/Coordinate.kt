a9b27b4af724c9f0b7e6d39fcc4ecce939e450e7
Jundertag <jaydenmacarthur3@gmail.com>
2026-03-03 18:20:23 -0400

    refactor: deleting useless typing classes

--- app/src/main/java/com/jayden/locationmanager/model/location/Coordinate.kt
+++
@@ -1,8 +0,0 @@
-package com.jayden.locationmanager.model.location
-
-data class Coordinate(
-    val latitude: Double,
-    val longitude: Double,
-    val bearing: Float?,
-    val provider: String?
-)
--- app/src/main/java/com/jayden/locationmanager/model/nmea/NmeaUtcTime.kt
+++
@@ -1,8 +0,0 @@
-package com.jayden.locationmanager.model.nmea
-
-data class NmeaUtcTime(
-    val hours: Int,
-    val minutes: Int,
-    val seconds: Int,
-    val milli: Int = 0
-)
