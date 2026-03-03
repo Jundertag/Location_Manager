d1872457eeb8caa169880dd9ec77293798bc209a
Jundertag <jaydenmacarthur3@gmail.com>
2026-03-03 17:37:00 -0400

    cleanup: optimize imports

--- app/src/main/java/com/jayden/locationmanager/app/MainApp.kt
+++ app/src/main/java/com/jayden/locationmanager/app/MainApp.kt
@@ -1,13 +1,6 @@
 package com.jayden.locationmanager.app
 
 import android.app.Application
-import android.location.LocationManager
-import androidx.room.Room
-import com.jayden.locationmanager.data.repository.LocationRepo
-import com.jayden.locationmanager.data.repository.NmeaLogsRepo
-import com.jayden.locationmanager.data.source.AppLocationManager
-import com.jayden.locationmanager.data.source.AppNmeaLogsListener
-import com.jayden.locationmanager.data.source.room.NmeaLogsDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
